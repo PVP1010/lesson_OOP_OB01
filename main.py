3a3d3c01d6c86ed8aa2e48d2935a2ed02dc887e1
PVP1010 <Valera10102791@mail.ru>
2024-09-27 15:41:15 +0700

    create class objects

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-class Warrior():
+class Warrior():                                            # создание класса Воина
     def __init__(self,name, power, endurance, hair_color):  # метод создания обьекта ( с атрибутами)
-        self.name = name                                    # имя объекта
-        self.power = power                                  # сила объекта
-        self.endurance = endurance                          # выносливость объекта
-        self.hair_color = hair_color                        # цвет волос  объекта
+        self.name = name                                    # имя объекта          (характеристика)
+        self.power = power                                  # сила объекта         (характеристика)
+        self.endurance = endurance                          # выносливость объекта (характеристика)
+        self.hair_color = hair_color                        # цвет волос  объекта  (характеристика)
 
     def sleep(self):                                        # метод спать
         print(f"{self.name} лёг спать")
@@ -25,3 +25,31 @@ class Warrior():
         print(f"цвет волос воина - {self.hair_color}")
         print(f"сила воина - {self.power}")
         print(f"выносливость воина - {self.endurance}")
+
+war1 = Warrior(name= "Стёпа", power= 76, endurance= 54, hair_color= "коричневый")
+war2 = Warrior(name= "Егор", power= 45, endurance= 23, hair_color= "блонд")
+
+print(war1.name)
+print(f"сила - {war1.power}")
+print(f"выносливость - {war1.endurance}")
+print(f"цвет волос - {war1.hair_color}")
+
+
+war1.sleep()
+war1.eat()
+war1.attack()
+war1.walk()
+war1.info()
+print(war1.info())
+
+print(war2.name)
+print(war2.power)
+print(war2.endurance)
+print(war2.hair_color)
+
+war2.sleep()
+war2.eat()
+war2.attack()
+war2.walk()
+war2.info()
+print(war2.info())
